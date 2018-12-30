3a1301a3aa90e9a1c7ca4a177f5cb34e63b0776b
texteng <stephen.teng@deyogroup.com>
2018-12-29 19:21:39 -0600

    can flip between different words, and see vocab and defnitions

---
+++ flashcards-react/src/components/Flashcard.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import '../css/Flashcards.css';
+import '../css/Flashcards.css'
+
+class Flashcard extends Component {
+  render() {
+    return (
+        <div className="card" onClick={() => this.flipCard()}>
+            {/* <div id = "definition" className= "card-text" onClick={() => this.flipCardtoDefinition()}> */}
+            <div id = "definition" className= "card-text">
+                <h2 className="display-5">{this.props.term}</h2>
+            </div>
+            {/* <div id = "term" className= "card-text d-none"  onClick={() => this.flipCardtoTerm()}> */}
+            <div id = "term" className= "card-text d-none">
+                <p>{this.props.definition}</p>
+            </div>
+        </div>
+    );
+  }
+  
+  //Helper Functions
+
+    flipCard = () => {
+      let cardDefinition = document.getElementById("definition");
+      let cardTerm = document.getElementById("term");
+      if(!cardDefinition.classList.contains("d-none")){
+        cardDefinition.classList.add("d-none");
+        cardTerm.classList.remove("d-none");
+      }
+      else if(!cardTerm.classList.contains("d-none")){
+        cardDefinition.classList.remove("d-none");
+        cardTerm.classList.add("d-none");
+      }
+
+    }
+
+}
+    
+    export default Flashcard;
+    
--- flashcards-react/src/components/Flashcards.jsx
+++ flashcards-react/src/components/Flashcards.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 // import '../css/Flashcards.css';
-import '../css/Flashcards.css'
+import '../css/Flashcards.css';
+import Flashcard from './Flashcard';
 
 class Flashcards extends Component {
+    state = {
+        vocabItem: [
+            {term: "Chicken", defintion: "Cluck, Cluck"},
+            {term: "Duck", defintion: "Quack, Quack"},
+            {term: "Cow", defintion: "Moo!"},
+            {term: "Pig", defintion: "Oink"},
+            {term: "Horse", defintion: "Nay"},
+        ],
+        wordDisplayIndex : 0
+    }
+
   render() {
     return (
     <React.Fragment>
@@ -13,13 +25,35 @@ class Flashcards extends Component {
         </div>
       </div>
       <div id="flashcards" className="row">
-        <div className = "col-8 offset-2">
-            <div id="flashcard_display">Flashcards</div>
+        <div id="flashcard_display" className = "col col-sm-8 offset-sm-2">
+            <Flashcard 
+                term= {this.state.vocabItem[this.state.wordDisplayIndex].term}
+                definition= {this.state.vocabItem[this.state.wordDisplayIndex].defintion}
+            />
+        </div>
+        <div className="navigation col-12">
+            <button className="btn btn-success" onClick={this.movetoPreviousTerm}>Previous Term</button>
+            <button className="btn btn-success" onClick={this.movetoNextTerm}>Next Term</button>
         </div>
       </div>
     </React.Fragment>
     );
   }
+
+movetoPreviousTerm = () => {
+    let currentState = this.state;
+    (currentState.wordDisplayIndex !== 0) ? currentState.wordDisplayIndex-- : currentState.wordDisplayIndex = currentState.vocabItem.length-1;
+    
+    this.setState(currentState);
 }
 
+movetoNextTerm = () => {
+    let currentState = this.state;
+    (currentState.wordDisplayIndex < currentState.vocabItem.length-1) ? currentState.wordDisplayIndex++ : currentState.wordDisplayIndex = 0;
+
+    this.setState(currentState);
+}
+
+
+}
 export default Flashcards;
